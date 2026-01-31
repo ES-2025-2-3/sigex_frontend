b39de3b3bff21d1ad4f5c4b936308b68ae4ac39d
arthur-vasco7 <arthur.vasconcelos.rego.barros@ccc.ufcg.edu.br>
2026-01-30 21:09:21 -0300

    componente spinner

--- src/commons/components/LoadingSpinner.tsx
+++ src/commons/components/LoadingSpinner.tsx
@@ -1,10 +1,9 @@
 import "./LoadingSpinner.css";
 
 interface LoadingSpinnerProps {
-    color?: string;
     size?: "small" | "medium" | "large";
 }
 
-export default function LoadingSpinner({color = "red", size = "medium",}: LoadingSpinnerProps) {
-    return <div className = "loading-spinner"/>;
+export default function LoadingSpinner({size = "medium"}: LoadingSpinnerProps) {
+    return <div className= {`loading-spinner loading-spinner--${size}`} />;
 }
--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -92,7 +92,7 @@ const HomePage = observer(() => {
 
           {loading ? (
             <div className="flex justify-center items-center py-20">
-                <LoadingSpinner />
+                <LoadingSpinner size="medium"/>
             </div>
           ) : (
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-10">
